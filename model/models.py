2707ce3f5d6a8fd64174f839731f60297973e97a
wuweipower <1752640695@qq.com>
2024-09-25 23:38:37 +0800

    add simple model definition

---
+++ model/models.py
@@ -0,0 +1,34 @@
+'''
+the networks that uaually used to train 
+'''
+
+import torch
+from torch import nn
+class MLP(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fcc = nn.Linear(64, output_dim)
+        
+
+    def forward(self):
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class CNN(nn.Module):
+
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(32 * 14 * 14, 10)
+    
+    def forward(self):
+        x = torch.relu(self.conv1(x))
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        return x
