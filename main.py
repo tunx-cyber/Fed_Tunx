7c8369a1b3259d0737fba872fdb634939b4f8880
wuweipower <1752640695@qq.com>
2024-09-29 21:26:20 +0800

    make a fedavg test

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 For test
 '''
 import data.data as d
-import FedGA.client as client
+import utils.client as client
 import model.models as models
 import torch
 import unittest
 import numpy as np
+import benchmark.fedavg as avg
 class TestFed(unittest.TestCase):
-    @unittest.skip("skip clinet test")
+    @unittest.skip("clinet test pass")
     def test_client(self):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         mnist={}
@@ -23,6 +24,7 @@ class TestFed(unittest.TestCase):
         c.train()
         c.test()
     
+    @unittest.skip("noniid setting pass")
     def test_dirichlet(self):
         import utils.Noniid as noniid
         dist = noniid.dirichlet_setting(10, 6000)
@@ -30,6 +32,13 @@ class TestFed(unittest.TestCase):
 
         dist = noniid.iid_setting(10, 6000)
         print(dist)
+
+    @unittest.skip("fedavg setting pass")
+    def test_fedavg(self):
+        model=models.MLP(28*28,10)
+        dataset_name="mnist"
+        server = avg.FedAvg(num_clients=20, model=model,dataset_name=dataset_name)
+        server.run()
 
 if __name__ == '__main__':
     unittest.main()
