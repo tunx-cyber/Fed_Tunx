4692ccc821570b738e727c79daa3d1309f915327
wuweipower <1752640695@qq.com>
2024-09-29 21:25:06 +0800

    add fedavg test

---
+++ benchmark/fedavg.py
@@ -0,0 +1,61 @@
+import utils.client as Client
+import utils.Noniid as noniid
+import data.data as data
+import torch
+import random
+import copy
+from torch.utils.data import DataLoader
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+class FedAvg:
+    def __init__(self, num_clients, model, dataset_name):
+        feddata = data.FedData(dataset_name=dataset_name)
+
+        self.train_set = feddata.get_training_data()
+        self.test_set  = feddata.get_test_data()
+        train_data_dist = noniid.dirichlet_setting(num_clients=num_clients, data_size=len(self.train_set))
+        test_data_dist = noniid.dirichlet_setting(num_clients=num_clients, data_size=len(self.test_set))
+        
+        self.global_model = model.to(device)
+        
+        self.clients = [Client.Client(model=copy.deepcopy(self.global_model).to(device),
+                                dataset={ 
+                                        "train": torch.utils.data.Subset(self.train_set, range(train_data_dist[i][0], train_data_dist[i][1])), 
+                                        "test": torch.utils.data.Subset(self.test_set, range(test_data_dist[i][0], test_data_dist[i][1]))
+                                        }, 
+                                id = i ) for i in range(num_clients)]
+        self.num_clients = num_clients
+        
+
+    #self.model.state_dict() 可以使用key()还有items()
+    def run(self):
+        self.test()
+        participants =  random.sample(range(0, self.num_clients), 2)
+        weights = []
+        for idx in participants:
+            w = self.clients[idx].train()
+            weights.append(w)
+        
+        client_sizes = [self.clients[idx].data_size for idx in participants]
+        total_size = sum(client_sizes)
+        with torch.no_grad():
+            for name, param in self.global_model.named_parameters():
+                weight_sum = sum(weights[i][name] *client_sizes[i]/total_size  for i in range(len(weights)))
+                param.data = weight_sum
+        self.test()
+    def test(self):
+        test_set = torch.utils.data.Subset(self.test_set,
+                                           random.sample(range(0, len(self.test_set)),500))
+        test_loader = DataLoader(dataset=test_set,batch_size=64)
+        self.global_model.eval()
+        with torch.no_grad():
+            correct = 0
+            total = 0
+
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                outputs = self.global_model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+            print(f'global_test_acc: {100 * correct / total:.2f}%')
+
