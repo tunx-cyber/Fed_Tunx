b896e5b916073a1846885b8ac59afd78fad670ce
wuweipower <1752640695@qq.com>
2024-10-11 15:08:10 +0800

    modify some parameters

--- utils/client.py
+++ utils/client.py
@@ -7,9 +7,9 @@ class Client:
     def __init__(self, model : nn.Module, dataset : dict, id = 0) -> None:
         self.model = model
         self.id = id
-        self.batch_size = 64
+        self.batch_size = 32
         self.size = len(dataset)
-        self.epoch = 10
+        self.epoch = 5
         self.train_data = dataset["train"]
         self.test_data = dataset["test"]
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -24,6 +24,7 @@ class Client:
         train_loader = DataLoader(dataset=self.train_data, batch_size= self.batch_size, shuffle=True)
 
         for _ in range(self.epoch):
+            #一个epoch就会遍历所有的数据，只不过一次处理的次数等于batchsize
             for img, label in train_loader:
                 img, label = img.to(self.device), label.to(self.device)
                 optimizer.zero_grad()
@@ -31,7 +32,7 @@ class Client:
                 loss = criterion(output, label)
                 loss.backward()
                 optimizer.step()
-            print(f'clinet {self.id} Epoch [{_+1}/{self.epoch}], Loss: {loss.item():.4f}')
+            print(f'client {self.id} Epoch [{_+1}/{self.epoch}], Loss: {loss.item():.4f}')
         
         origin_param = origin_model.state_dict()
         local_param = self.model.state_dict()
@@ -55,6 +56,6 @@ class Client:
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
-            print(f'test_acc: {100 * correct / total:.2f}%')
+            print(f'client{self.id} test_acc: {100 * correct / total:.2f}%')
             return correct/total
             
