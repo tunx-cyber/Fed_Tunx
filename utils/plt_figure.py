2b1257d339b005b2c2feb512d8f773075be511b6
wuweipower <1752640695@qq.com>
2024-10-12 17:33:03 +0800

    seperate the plotting functions

---
+++ utils/plt_figure.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+def draw_clients_acc_bar(num_clients, accs):
+    x = range(num_clients)
+    plt.bar(x, accs, color='skyblue')
+    plt.title("local test accuracy")
+    plt.xlabel("client id")
+    plt.ylabel("accuracy")
+    plt.show()
+
+def draw_trainning_acc(rounds, accs, title):
+    plt.plot(rounds, accs, label='test_acc', color='blue')
+    plt.title(title)
+    plt.xlabel('round')
+    plt.ylabel('accuracy')
+    plt.show()
