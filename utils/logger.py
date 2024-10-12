9e3953e1d3a20bb9b52554268990b229d1594d4e
wuweipower <1752640695@qq.com>
2024-10-12 20:53:53 +0800

    add a logger

---
+++ utils/logger.py
@@ -0,0 +1,12 @@
+import logging  
+  
+# 配置日志记录  
+logging.basicConfig(
+    level=logging.INFO,  # 设置日志级别为INFO  
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # 设置日志格式  
+    datefmt='%Y-%m-%d %H:%M:%S',  # 设置日期格式  
+    filename='./log/log.txt'
+)  
+  
+# 创建一个日志记录器  
+logger = logging.getLogger(__name__)
